9b4c83cb3984eaf77aeb46f332ac8bb8ff0d1219
PeterKellett <peterwkellett@gmail.com>
2021-02-02 21:13:52 +0000

    Game logic applied

--- run.py
+++ run.py
@@ -8,27 +8,13 @@ app.secret_key = 'some_secret'
 
 @app.route("/")
 def index():
-    cards = 16
-    x = 0
-    number_list = []
-    while x < cards:
-        number_list.append(x)
-        x = x + 1
-    print(number_list)
-    return render_template('index.html', page_title="Page 1", number_list=number_list)
+    return render_template('index.html', page_title="Page 1")
 
 
 @app.route("/page_2")
 def page_2():
     # flash("Button clicked")
-    cards = 16
-    x = 0
-    number_list = []
-    while x < cards:
-        number_list.append(x)
-        x = x + 1
-    print(number_list)
-    return render_template("page_2.html", page_title="Page 2", number_list=number_list)
+    return render_template("page_2.html", page_title="Page 2")
 
 
 if __name__ == "__main__":
