d5363f22c5e21ffc3d1ccefd43a444a15b4845d6
PeterKellett <peterwkellett@gmail.com>
2021-02-03 16:12:38 +0000

    page 1 game logic complete

--- run.py
+++ run.py
@@ -1,20 +1,43 @@
 import os
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, url_for, redirect
+from flask_pymongo import PyMongo
 
+if os.path.exists("env.py"):
+    import env as config
+
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.secret_key = 'some_secret'
+app.secret_key = os.getenv("SECRET", "randomstring123")
+
+app.config["MONGO_DBNAME"] = 'bingo'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', page_title="Page 1")
+    cards = list(mongo.db.ladies.find())
+    x = 0
+    y = 0
+    for card in cards:
+        card.update({'x': x, 'y': y})
+        print(card)
+        x += 1
+        if x == 4:
+            x = 0
+            y += 1
+    return render_template('index.html',
+                           page_title="Page 1",
+                           cards=cards)
 
 
 @app.route("/page_2")
 def page_2():
     # flash("Button clicked")
-    return render_template("page_2.html", page_title="Page 2")
+    return render_template("page_2.html",
+                           page_title="Page 2")
 
 
 if __name__ == "__main__":
